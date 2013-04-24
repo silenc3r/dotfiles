ebcbd098b15bffd4bb3fa4a5b8f79792ea940ab2
silenc3r <dawidzych@fastmail.fm>
2013-04-24 21:38:40 +0200

    modified CDPATH, added VISUAL

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,9 @@ set fish_greeting ""
 set PATH $PATH /home/silencer/.gem/ruby/2.0.0/bin
 set PATH $PATH /home/silencer/bin
 
-set -x CDPATH '.'
+set -x CDPATH "." "$HOME" $CDPATH
+
 set -x EDITOR vim
+set -x VISUAL vim
+
 set -x GOPATH $HOME/Dokumenty/code/go
