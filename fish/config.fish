a234e168b78f95281672e1ff4d3498ba64f785be
silenc3r <dawidzych@fastmail.fm>
2013-04-24 15:09:59 +0200

    added greeting, editor, gopath

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,7 @@
-set PATH $PATH /home/silencer/.gem/ruby/2.0.0/bin /home/silencer/bin
+set fish_greeting ""
+
+set PATH $PATH /home/silencer/.gem/ruby/2.0.0/bin
+set PATH $PATH /home/silencer/bin
+
+set -x EDITOR vim
+set -x GOPATH $HOME/Dokumenty/code/go
