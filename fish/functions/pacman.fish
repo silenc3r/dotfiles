e16cb6f28e9da4ffd5086363b35ae818be567941
silenc3r <dawidzych@fastmail.fm>
2013-05-12 15:28:36 +0200

    finally works (internal fish bug)

--- fish/functions/pacman.fish
+++ fish/functions/pacman.fish
@@ -1,10 +1,8 @@
-function pac
+function pacman
     switch $argv[1]
         case '-D' '-R*' '-U*' '-S' '-Sc*' '-Sd*' '-Su*' '-Sw*' '-Sy*'
             /usr/bin/sudo /usr/bin/pacman $argv
-            return 0
         case '*'
             /usr/bin/pacman $argv
-            return 0
     end
 end
