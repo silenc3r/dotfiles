6a90a2fecf55f94e60b11fad3a3a65b413a5d092
Dawid Zych <dawidzych@fastmail.fm>
2014-12-03 18:44:57 +0100

    Major cleanup

--- fish/functions/pacman.fish
+++
@@ -1,8 +0,0 @@
-function pacman
-    switch $argv[1]
-        case '-D' '-R*' '-U*' '-S' '-Sc*' '-Sd*' '-Su*' '-Sw*' '-Sy*'
-            /usr/bin/sudo /usr/bin/pacman $argv
-        case '*'
-            /usr/bin/pacman $argv
-    end
-end
