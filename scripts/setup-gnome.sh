dd5a70c7979fb4b17b3631f61fd217f92ce4d57f
Dawid Zych <dawidzych@fastmail.fm>
2014-12-15 23:38:26 +0100

    update

--- scripts/setup-gnome.sh
+++ scripts/setup-gnome.sh
@@ -11,15 +11,15 @@ dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-7 "['<Shift><Sup
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-8 "['<Shift><Super>asterisk']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-9 "['<Shift><Super>parenleft']"
 # Super+number switches to workspace
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "[<Super>1']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "[<Super>2']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "[<Super>3']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "[<Super>4']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "[<Super>5']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "[<Super>6']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "[<Super>7']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "[<Super>8']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "[<Super>9']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Super>1']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Super>2']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Super>3']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Super>4']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>5']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>6']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Super>7']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Super>8']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "['<Super>9']"
 
 # terminal
 dconf write /org/gnome/terminal/legacy/dark-theme true
