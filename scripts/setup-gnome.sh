2ac8df87024cca046d0bd62ef2eae21567980a5b
Dawid Zych <dawidzych@fastmail.fm>
2015-03-13 12:23:02 +0100

    improve colors and italics support in vim and tmux

--- scripts/setup-gnome.sh
+++ scripts/setup-gnome.sh
@@ -28,13 +28,7 @@ dconf write /org/gnome/terminal/legacy/dark-theme true
 dconf write /org/gnome/terminal/legacy/default-show-menubar false
 dconf write /org/gnome/terminal/legacy/mnemonics-enabled false
 dconf write /org/gnome/terminal/legacy/schema-version 3
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-1 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-2 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-3 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-4 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-5 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-6 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-7 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-8 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-9 disabled
-dconf write /org/gnome/terminal/legacy/keybindings/switch-to-tab-10 disabled
+
+# OTHER SETTINGS
+# custom terminal command:
+# sh -c 'export TERM=gnome-256color; exec bash'
