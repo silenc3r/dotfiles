4d0be5fd68fcc7fd89210ac7ce4e0a4289baade4
Dawid Zych <dawidzych@fastmail.fm>
2016-03-21 19:51:10 +0100

    update gen_terminfo

--- tmux/gen_terminfo.sh
+++ tmux/gen_terminfo.sh
@@ -8,6 +8,6 @@ infocmp "$screen_terminfo" | sed \
   -e 's/%?%p1%t;3%/%?%p1%t;7%/' \
   -e 's/smso=[^,]*,/smso=\\E[7m,/' \
   -e 's/rmso=[^,]*,/rmso=\\E[27m,/' \
-  -e '$s/$/ sitm=\\E[3m, ritm=\\E[23m,/' > /tmp/screen.terminfo
+  -e '$s/$/ sitm=\\E[3m, ritm=\\E[23m,/' > /tmp/screen-256color.terminfo
 
-tic /tmp/screen.terminfo
+tic /tmp/screen-256color.terminfo
