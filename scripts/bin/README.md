3cb48b75532fb1bf7022239b6f6661996bbcf1de
Dawid Zych <dawid.zych@yandex.com>
2020-05-14 11:44:03 +0200

    remove scripts

--- scripts/bin/README.md
+++
@@ -1,3 +0,0 @@
-# ~/bin/
-
-Useful scripts either written by myself or found on the internet (mostly the latter)
--- scripts/bin/transmission-remove-after-completed.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Remove torrents after completion
-
-transmission-remote --torrent $TR_TORRENT_ID --remove
